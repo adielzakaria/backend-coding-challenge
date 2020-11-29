007603da5d8dfcb6a48b592ce14b50b29dfa1e03
adielzakaria <34061897+adielzakaria@users.noreply.github.com>
2020-11-29 13:17:32 +0100

    add comparer function

---
+++ src/helpers/comparer.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+//when called using Object.entries the index 0 is the name of the language the index 1 contains  the repository object
+export function compareRepositories(object1, object2, key) {
+    const obj1 = object1[1][key]
+    const obj2 = object2[1][key]    
+    if (obj1 < obj2) {
+      return -1
+    }
+    else if (obj1 > obj2) {
+      return 1
+    }
+    return 0
+  }
