11dda121b6b9006016636caf79e4b07bc438db1b
adielzakaria <34061897+adielzakaria@users.noreply.github.com>
2020-12-24 19:35:56 +0100

    rename compare helper and change its body

--- src/helpers/comparer.ts
+++ src/helpers/comparer.ts
@@ -1,6 +1,4 @@
 //when called using Object.entries the index 0 is the name of the language the index 1 contains  the repository object
-export function compareRepositories(object1, object2, key) {
-  const obj1 = object1[1][key];
-  const obj2 = object2[1][key];
-  return obj1 > obj2 ? 1 : obj1 < obj2 ? -1 : 0;
+export function compareObjectsByKey(object1, object2, key) {
+  return object1[key] > object2[key] ? 1 : object1[key] < object2[key] ? -1 : 0;
 }
