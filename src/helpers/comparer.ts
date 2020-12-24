f4bf74afe96625fb6f163464905f9e01fc58cbce
adielzakaria <34061897+adielzakaria@users.noreply.github.com>
2020-12-24 20:48:32 +0100

    delete unused file

--- src/helpers/comparer.ts
+++
@@ -1,4 +0,0 @@
-//when called using Object.entries the index 0 is the name of the language the index 1 contains  the repository object
-export function compareObjectsByKey(object1, object2, key) {
-  return object1[key] > object2[key] ? 1 : object1[key] < object2[key] ? -1 : 0;
-}
