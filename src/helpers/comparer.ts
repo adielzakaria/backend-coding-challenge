f43ab8dbfaceb7624d61b813ec4803d54550af74
adielzakaria <34061897+adielzakaria@users.noreply.github.com>
2020-12-24 14:35:00 +0100

    replace multiple if's with ternary operator instead

--- src/helpers/comparer.ts
+++ src/helpers/comparer.ts
@@ -2,10 +2,5 @@
 export function compareRepositories(object1, object2, key) {
   const obj1 = object1[1][key];
   const obj2 = object2[1][key];
-  if (obj1 < obj2) {
-    return -1;
-  } else if (obj1 > obj2) {
-    return 1;
-  }
-  return 0;
+  return obj1 > obj2 ? 1 : obj1 < obj2 ? -1 : 0;
 }
