428b276c7c9292e62a777158542efb531daab29f
adielzakaria <34061897+adielzakaria@users.noreply.github.com>
2020-12-24 14:34:14 +0100

    fix styling

--- src/client/clientController.ts
+++ src/client/clientController.ts
@@ -1,14 +1,13 @@
-/* eslint-disable prettier/prettier */
-import { Controller, Get, Render } from '@nestjs/common'
-import { ClientService } from './clientService'
+import { Controller, Get, Render } from '@nestjs/common';
+import { ClientService } from './clientService';
 
 @Controller()
 export class ClientController {
-    constructor(private readonly clientService: ClientService) {}
+  constructor(private readonly clientService: ClientService) {}
 
   @Get()
   @Render('index')
-   async getTrends() {
-    return await this.clientService.getTrend() 
+  async getTrends() {
+    return await this.clientService.getTrend();
   }
 }
